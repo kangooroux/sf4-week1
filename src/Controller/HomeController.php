325aa5cc75a67b20996677e923796ff00a7ff9cb
kangooroux <bousquetalexandrepro@gmail.com>
2020-08-11 12:01:46 +0200

    Installation twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,21 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
+     * @param int $id
      * @Route("/home/{id<\d+>}", name="home")
      */
     //@Route("/home/{id<\d+>}", name="home") <- regex pour vérifier que l'id est un entier
     //@Route("/blog/{page}", name="blog_list", requirements={"page"="\d+"}) <- regex pour vérifier que l'id est un entier
-    public function index($id)
+    public function index($id, Request $request, SessionInterface $session)
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/HomeController.php',
             'id' => $id,
+            'get' => $request->query->get('nuke', 'default') ,
+            'session' => get_class($session),
         ]);
     }
 }
